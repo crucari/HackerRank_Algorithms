ff5476a9fe7f9122742f721d75563eda2481c442
Carina Cruz <lecruz@bu.edu>
2018-05-06 00:18:45 +0000

    pseudocode triplets

---
+++ Compare_the_Triplets.c
@@ -0,0 +1,6 @@
+  /*Compare a and b
+    /*If a is greater than b, Alice(A) gets 1 point
+    /*If a is equal to b, no points are awarded
+    /*If a is less than b, Bob(B) gets 1 point
+    /*Print Alice and bob's comparison score as an array. Use array.join
+     */
