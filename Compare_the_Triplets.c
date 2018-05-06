efa1a4f5f807df2f54924bf01f076f8d6556de9d
Carina Cruz <lecruz@bu.edu>
2018-05-06 00:20:43 +0000

    defining variables

--- Compare_the_Triplets.c
+++ Compare_the_Triplets.c
@@ -4,3 +4,8 @@
     /*If a is less than b, Bob(B) gets 1 point
     /*Print Alice and bob's comparison score as an array. Use array.join
      */
+
+function solve(a0, a1, a2, b0, b1, b2) {
+//define variables
+var A = 0;  //A=Alice's comparison score
+var B= 0;   //B= Bob's comparison score
